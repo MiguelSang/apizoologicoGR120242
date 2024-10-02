5874d418f84765ba48d05b96f16d3de189a5714b
aaaa <example@gmail.com>
2024-10-02 17:20:19 -0500

    se termino de agregar el token

--- src/routes/animal.js
+++ src/routes/animal.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const verifyToken = require('./validate_token');
 const router = express.Router(); //manejador de rutas de express
 const animalSchema = require("../models/animal");
 //Nuevo animal
@@ -9,4 +10,10 @@ router.post("/animals", (req, res) => {
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }));
 });
+//Consultar todos los animales 
+router.get("/animals", verifyToken, (req, res) => {
+    animalSchema.find()
+    .then((data) => res.json(data))
+    .catch((error) =>res.json({ message: erro}));
+})
 module.exports = router;
